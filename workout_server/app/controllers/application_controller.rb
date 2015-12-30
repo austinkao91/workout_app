5c01eaa1e9c61095fc27716d56ca47352e468d97
Austin Kao <austinkao91@gmail.com>
2015-12-29 22:12:13 -0800

    moved setting csrf to session controller

--- workout_server/app/controllers/application_controller.rb
+++ workout_server/app/controllers/application_controller.rb
@@ -3,14 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   after_filter :set_csrf_cookie_for_ng
-
   def set_csrf_cookie_for_ng
-    debugger
+
     cookies['XSRF-TOKEN'] = session[:_csrf_token] if protect_against_forgery?
   end
   protected
   def verified_request?
-    debugger
+
     super || valid_authenticity_token?(session, request.headers['X-XSRF-TOKEN'])
   end
 end
--- workout_server/app/controllers/user/sessions_controller.rb
+++ workout_server/app/controllers/user/sessions_controller.rb
@@ -1,8 +1,10 @@
 class User::SessionsController < Devise::SessionsController
   respond_to :json, :html
 
+  after_filter :set_csrf_cookie_for_ng, only: [:create, :destroy]
+
+
   def create
-    debugger
     sign_out(:user) if current_user
     self.resource = warden.authenticate!(auth_options)
     set_flash_message(:notice, :signed_in) if is_flashing_format?
@@ -10,5 +12,11 @@ class User::SessionsController < Devise::SessionsController
     yield resource if block_given?
     respond_with resource, location
     after_sign_in_path_for(resource)
+  end
+
+  protected
+  def set_csrf_cookie_for_ng
+    cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+
   end
 end
